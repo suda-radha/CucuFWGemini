d7e8d04f86f469bf8474b298137d54a7ddde435f
suda-radha <suda.radha22@gmail.com>
2026-03-01 14:58:50 -0600

    updated runner

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/resources/features/Shop.feature",
+		features = "src/test/resources/features",
 		glue = "StepDefinitions",
 		plugin = {"pretty", 
 				  "html:target/cucumber-reports.html", 
